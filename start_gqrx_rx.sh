a8adbeca590aee8268bf8ab16ff0f52fa2adef9b
Mark Jessop <vk5qi@rfhead.net>
2020-01-22 19:31:51 +1030

    Make GQRX demodulator have wider frequency estimator limits (100 Hz to 10 kHz)

--- start_gqrx_rx.sh
+++ start_gqrx_rx.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-nc -l -u localhost 7355 | ./horus_demod -m binary - - | python horusbinary.py --stdin $@
+nc -l -u localhost 7355 | ./horus_demod -m binary --fsk_lower=100 --fsk_upper=10000 - - | python horusbinary.py --stdin $@
